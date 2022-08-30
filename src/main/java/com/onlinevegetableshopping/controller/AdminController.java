194bb0097be7f5fcf66e2302f458d9eb96c5c269
iakhil <iakhil@INLEN8520041979.corp.capgemini.com>
2022-08-30 15:01:41 +0530

    Issue has been resolved, able to push perfectly

--- src/main/java/com/onlinevegetableshopping/controller/AdminController.java
+++ src/main/java/com/onlinevegetableshopping/controller/AdminController.java
@@ -49,6 +49,7 @@ public class AdminController {
 
 	}
 
+	
 	// requests the controller to delete by vegetable id in vegetable store which is done by admin
 
 	// http://localhost:8091/onlinevegetableshopping/admin/deletebyid/
